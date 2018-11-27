b7ff8830ccde35b14ec962d08c9630956b6f01cc
jewelahamed <jewelahamed58@gmail.com>
2018-11-27 14:59:30 -0500

    hello G

---
+++ src/jewel22.java
@@ -0,0 +1,35 @@
+public class jewel22 {
+
+
+
+    // class loop
+
+        public static void main (String [] args) {
+
+
+            System.out.println("Do-While loop");
+
+            int y = 3;
+            do {
+                System.out.println("current y:" + y);
+                y=y-1;
+
+            }while (y>0);
+            System.out.println("Final x:"+ y);
+            System.out.println();
+
+            System.out.println("for Loop");
+
+            for(int i= 3; i>0; i--) {
+                System.out.println("current i:" +i);
+
+
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+
